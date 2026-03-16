f6bf68c8d09158d2819aa2c881b04fa5c0ad5c3d
ray <raisoares9155@gmail.com>
2026-03-16 17:43:56 -0300

    segundo exercicio

--- main.py
+++ main.py
@@ -1,11 +1,53 @@
-qtde = int(input("Digite a quantidade de notas: "))
-notas = []
-pesos = []
-for i in range(qtde):
-    nota = float(input(f"Digite a {i+1}ª nota: "))
-    peso = float(input("Digite o peso da respectiva nota: "))
-    notas.append(nota * peso)
-    pesos.append(peso)
-    print('\n')
+vez = "n"
 
-print(f"A média ponderada é: {sum(notas) / sum(pesos):.2f}")
+def soma(a, b):
+    return a + b
+
+def subtracao(a, b):
+    return a - b
+
+def multiplicacao(a, b):
+    return a * b
+
+def divisao(a, b):
+    if b != 0:
+        return a / b
+    else:
+        return "Erro: Divisão por zero"
+    
+def potencia(a, b):
+    return a ** b
+
+while vez.lower() == "n":
+
+    try:
+        valor1 = float(input("Digite o primeiro valor: "))
+        valor2 = float(input("Digite o segundo valor: "))
+
+    except ValueError:
+        print("Valor inválido. Por favor, digite um número.")
+        continue
+    
+    operacao = input("Digite o simbolo da operação (+, -, *, /, **): ")
+
+    if operacao == "+":
+        resultado = soma(valor1, valor2)
+
+    elif operacao == "-":
+        resultado = subtracao(valor1, valor2)
+
+    elif operacao == "*":
+        resultado = multiplicacao(valor1, valor2)
+
+    elif operacao == "/":
+        resultado = divisao(valor1, valor2)
+
+    elif operacao == "**":
+        resultado = potencia(valor1, valor2)
+
+    else:
+        resultado = "Operação inválida"
+
+    print("Resultado: ", resultado)
+
+    vez = input("Deseja realizar outra operação? (s/n): ")
