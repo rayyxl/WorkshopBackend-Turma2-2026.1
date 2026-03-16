a30555599b78a0e22978d4943a94f9c9830ba9f3
ray <raisoares9155@gmail.com>
2026-03-16 16:46:21 -0300

    primeiro exercicio

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print('Hello, World!')
+qtde = int(input("Digite a quantidade de notas: "))
+notas = []
+pesos = []
+for i in range(qtde):
+    nota = float(input(f"Digite a {i+1}ª nota: "))
+    peso = float(input("Digite o peso da respectiva nota: "))
+    notas.append(nota * peso)
+    pesos.append(peso)
+    print('\n')
+
+print(f"A média ponderada é: {sum(notas) / sum(pesos):.2f}")
